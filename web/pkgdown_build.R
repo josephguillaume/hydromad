3460f87866760527a4273724de8f35caf4ce0c17
buzacott <abuz5257@uni.sydney.edu.au>
2020-07-01 11:19:00 +1000

    use pkgdown

---
+++ web/pkgdown_build.R
@@ -0,0 +1,20 @@
+library(usethis)
+library(pkgdown)
+
+# Generate pkgdown website
+
+# Set up package to use pkgdown
+# usethis::use_pkgdown(destdir = 'web/pkgdown')
+# Destination can be changed in _pkgdown.yml
+
+# Build website
+# pkgdown::build_site()
+
+# Update pages
+# pkgdown::build_home()
+# pkgdown::build_articles() # Includes Getting Started
+# pkgdown::build_news()
+# pkgdown::build_reference()
+# pkgdown::build_tutorials()
+
+

6e62423bbb1a89519bf49285061725d5ac0d3e9a
mortisflux <j.ozsvar@gmail.com>
2020-07-06 11:23:31 +1000

    test-calibration-results now passing

--- tests/testthat/test-calibration-results.R
+++ tests/testthat/test-calibration-results.R
@@ -16,9 +16,9 @@ test_that("cwi+expuh optim gives reasonable result", {
   set.seed(0)
   fitx <- fitByOptim(modx)
   s <- summary(fitx)
-  expect_that(s$r.squared > 0.65, expect_true())
-  expect_that(s$r.sq.log > 0.8, expect_true())
-  expect_that(abs(s$rel.bias) < 0.01, expect_true())
+  expect_more_than(s$r.squared, 0.65)
+  expect_more_than(s$r.sq.log, 0.8)
+  expect_less_than(s$rel.bias, 0.01)
 })
 
 
@@ -33,10 +33,12 @@ test_that("cwi+armax optim/SRIV gives reasonable result", {
   set.seed(0)
   fitx <- fitByOptim(modx)
   s <- summary(fitx)
-  expect_that(s$r.squared > 0.75, expect_true())
-  expect_that(s$r.sq.log > 0.85, expect_true())
-  expect_that(abs(s$rel.bias) < 0.07, expect_true())
+  expect_more_than(s$r.squared, 0.75)
+  expect_more_than(s$r.sq.log, 0.85)
+  expect_less_than(abs(s$rel.bias), 0.07)
 })
+
+
 
 
 test_that("cmd+powuh SCE gives reasonable result", {
@@ -47,7 +49,7 @@ test_that("cmd+powuh SCE gives reasonable result", {
   set.seed(0)
   fitx <- fitBySCE(modx, control = list(maxit = 3))
   s <- summary(fitx)
-  expect_that(s$r.squared > 0.5, expect_true())
-  expect_that(s$r.sq.log > 0.6, expect_true())
-  expect_that(abs(s$rel.bias) < 0.06, expect_true())
+  expect_more_than(s$r.squared, 0.5)
+  expect_more_than(s$r.sq.log, 0.6)
+  expect_less_than(abs(s$rel.bias), 0.06)
 })

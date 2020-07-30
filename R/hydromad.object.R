31815d44809473877ff1a82af294f0508b5cb351
mortisflux <j.ozsvar@gmail.com>
2020-07-30 15:52:43 +1000

    Change width to width.cutoff in print.hydromad

--- R/hydromad.object.R
+++ R/hydromad.object.R
@@ -304,7 +304,7 @@ print.hydromad <-
     if (!is.null(x$rfit)) {
       cat(
         "Routing fit spec.:",
-        toString(deparse(x$rfit, control = c(), width = 500),
+        toString(deparse(x$rfit, control = c(), width.cutoff = 500),
           width = getOption("width")
         ), "\n"
       )
@@ -320,7 +320,7 @@ print.hydromad <-
     if (length(x$info.rfit) > 0) {
       cat(
         "\nRouting fit info: ",
-        toString(deparse(x$info.rfit, control = c(), width = 500),
+        toString(deparse(x$info.rfit, control = c(), width.cutoff = 500),
           width = getOption("width")
         ), "\n"
       )

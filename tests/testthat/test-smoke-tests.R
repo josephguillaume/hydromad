0ba1a701467bbfcdb08d27a1b6c1e4a55efa154d
mortisflux <j.ozsvar@gmail.com>
2020-08-03 09:51:48 +1000

    Change expect_is to expect_type

--- tests/testthat/test-smoke-tests.R
+++ tests/testthat/test-smoke-tests.R
@@ -8,14 +8,14 @@ ts90s <- window(Cotter$Q, start = "1990-01-01", end = "1999-12-31")
 
 ## Test that tsdiag.hydromad returns correct class
 test_that("can plot arima models with tsdiag", {
-  expect_is(tsdiag(arima(Cotter$P)), "NULL")
-  expect_is(tsdiag(arima(ts90s)), "NULL")
+  expect_type(tsdiag(arima(Cotter$P)), "NULL")
+  expect_type(tsdiag(arima(ts90s)), "NULL")
   expect_error(tsdiag(Cotter))
 })
 
 ## Test that xyplot.hydromad works returns correct class
 test_that("can plot data with xyplot plot", {
-  expect_is(xyplot(Cotter), "trellis")
-  expect_is(xyplot(ts90s), "trellis")
+  expect_type(xyplot(Cotter), "trellis")
+  expect_type(xyplot(ts90s), "trellis")
   expect_error(xyplot(arima(ts90s)))
 })
